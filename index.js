1f26b5587df5768641b934bd9a54455e1cdfbcc6
Rajkumarthakur88 <thakurraj8878@gmail.com>
2023-03-20 21:12:27 +0530

    new products route added

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
-
+const data = require('./data.json')
 server.use(middlewares);
 server.use(router);
+
 
 server.listen(port);
